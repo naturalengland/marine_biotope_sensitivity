682a420ac71e05b4622438974a87e6932f2f7680
Philip Haupt <philip.haupt@gmail.com>
2019-04-11 17:17:04 +0100

    update print text

--- functions/set_user_ops_act_choice.R
+++ functions/set_user_ops_act_choice.R
@@ -2,6 +2,6 @@
 
 choice <- OpsAct %>% filter(OperationCode == ops.number)
 print(paste0("Your choice, Operation Code: ", choice$OperationCode, ", operation name: ", 
-      choice$OperationName, ", Activity Code: ", choice$MainActCode))
+      choice$OperationName, ", Main Activity Code: ", choice$MainActCode))
 
 main.act.code <- paste0(choice$MainActCode,".")
