c0f6c14877bb51dd0de70073a7303e8746514e49
Philip Haupt <philip.haupt@gmail.com>
2019-04-18 11:58:00 +0100

    move commented section to bottom of function

--- functions/01_connect_to_ms_access_qry_data.R
+++ functions/01_connect_to_ms_access_qry_data.R
@@ -1,6 +1,6 @@
 read.access.db <- function(db.p = db.path, drv.p = drv.path){
         
-        #user defined operation filter (move this to the main script, global variables such taht the user choooses the operation in teh beginning)
+        #user defined operation filter (move this to the main script, global variables such that the user choooses the operation in teh beginning)
         operation <- as.character(ops.number)
         
         #specificy the connection to the database
@@ -10,41 +10,6 @@ read.access.db <- function(db.p = db.path, drv.p = drv.path){
         conn <- odbcDriverConnect(connection.path)
         
         
-        #populate qryEUNIS_ActPressureSens directly from the query in the database, if it fails attempt to reconstruct it from the tables in the database:
-        #qryEUNIS_ActPressSens <- try(sqlQuery(conn, paste("SELECT qryEUNIs_grp_act.ActivityCode, qryEUNIs_grp_act.ActivityName, tblEUNISPressure.PressureCode, tblPressureLUT.PressureName, qryEUNIs_grp_act.EUNISCode, tblEUNISLUT.EUNISName, tblSensitivityLUT.ActSensRank
-        #                                              FROM tblPressureLUT INNER JOIN (tblActivityPressure INNER JOIN ((tblEUNISLUT INNER JOIN qryEUNIs_grp_act ON tblEUNISLUT.EUNISCode = qryEUNIs_grp_act.EUNISCode) INNER JOIN (tblSensitivityLUT INNER JOIN tblEUNISPressure ON tblSensitivityLUT.EPSens = tblEUNISPressure.Sensitivity) ON qryEUNIs_grp_act.EUNISCode = tblEUNISPressure.EUNISCode) ON (tblEUNISPressure.PressureCode = tblActivityPressure.PressureCode) AND (tblActivityPressure.ActivityCode = qryEUNIs_grp_act.ActivityCode)) ON tblPressureLUT.PressureCode = tblActivityPressure.PressureCode
-        #                                                  WHERE (((tblSensitivityLUT.SensPriority)<'8') AND ((tblActivityPressure.APRelevant)='Yes'))
-        #                                                  ORDER BY qryEUNIs_grp_act.ActivityCode, tblEUNISPressure.PressureCode, qryEUNIs_grp_act.EUNISCode;"))) 
-        #if("try-error" %in% class(sqlQuery(conn, paste("SELECT qryEUNIs_grp_act.ActivityCode, qryEUNIs_grp_act.ActivityName, tblEUNISPressure.PressureCode, tblPressureLUT.PressureName, qryEUNIs_grp_act.EUNISCode, tblEUNISLUT.EUNISName, tblSensitivityLUT.ActSensRank
-        #                                              FROM tblPressureLUT INNER JOIN (tblActivityPressure INNER JOIN ((tblEUNISLUT INNER JOIN qryEUNIs_grp_act ON tblEUNISLUT.EUNISCode = qryEUNIs_grp_act.EUNISCode) INNER JOIN (tblSensitivityLUT INNER JOIN tblEUNISPressure ON tblSensitivityLUT.EPSens = tblEUNISPressure.Sensitivity) ON qryEUNIs_grp_act.EUNISCode = tblEUNISPressure.EUNISCode) ON (tblEUNISPressure.PressureCode = tblActivityPressure.PressureCode) AND (tblActivityPressure.ActivityCode = qryEUNIs_grp_act.ActivityCode)) ON tblPressureLUT.PressureCode = tblActivityPressure.PressureCode
-        #                                               WHERE (((tblSensitivityLUT.SensPriority)<'8') AND ((tblActivityPressure.APRelevant)='Yes'))
-        #                                               ORDER BY qryEUNIs_grp_act.ActivityCode, tblEUNISPressure.PressureCode, qryEUNIs_grp_act.EUNISCode;")))) {
-        #        qryEUNIS_ActPressSens <- read.csv("C:/Users/M996613/Phil/PROJECTS/Fishing_effort_displacement/2_subprojects_and_data/3_Other/NE/Habitat_sensitivity/qryhabsens/qryEUNIS_ActPressSens.txt")
-        #}
-        
-        
-        #NB NB NB NB NB NB KEY query!!!!!!!!!!!!!!!!!!!!!!! If IT FAILS DO THE BELOW - nbeeds to be changed to a try statement
-        # 3a if queries are in Access this step is NOT required, but can be run to VIEW the data; else skip to next step
-        #qryEUNIS_ActPressSens <- sqlQuery(conn, paste("SELECT qryEUNIs_grp_act.ActivityCode, qryEUNIs_grp_act.ActivityName, tblEUNISPressure.PressureCode, tblPressureLUT.PressureName, qryEUNIs_grp_act.EUNISCode, tblEUNISLUT.EUNISName, tblSensitivityLUT.ActSensRank
-        #                                              FROM tblPressureLUT INNER JOIN (tblActivityPressure INNER JOIN ((tblEUNISLUT INNER JOIN qryEUNIs_grp_act ON tblEUNISLUT.EUNISCode = qryEUNIs_grp_act.EUNISCode) INNER JOIN (tblSensitivityLUT INNER JOIN tblEUNISPressure ON tblSensitivityLUT.EPSens = tblEUNISPressure.Sensitivity) ON qryEUNIs_grp_act.EUNISCode = tblEUNISPressure.EUNISCode) ON (tblEUNISPressure.PressureCode = tblActivityPressure.PressureCode) AND (tblActivityPressure.ActivityCode = qryEUNIs_grp_act.ActivityCode)) ON tblPressureLUT.PressureCode = tblActivityPressure.PressureCode
-        #                                              WHERE (((tblSensitivityLUT.SensPriority)<'8') AND ((tblActivityPressure.APRelevant)='Yes'))
-        #                                              ORDER BY qryEUNIs_grp_act.ActivityCode, tblEUNISPressure.PressureCode, qryEUNIs_grp_act.EUNISCode;"))
-        #qryEUNIS_ActPressSens <- as.data.frame(sapply(X = qryEUNIS_ActPressSens, FUN = as.character), stringsAsFactors=FALSE)
-        
-        
-        #-------------------
-        ## Inspect data
-        ## List of Tables
-        #subset(sqlTables(conn), TABLE_TYPE == "TABLE") %>%
-        #        arrange(TABLE_NAME)
-        ## List of VIEW queries
-        #subset(sqlTables(conn), TABLE_TYPE == "VIEW")%>%
-        #        arrange(TABLE_NAME)
-        
-        
-        ## All below "building" queries, included, but only query 3 required, as long as the queries are stored in the Access database. IF not stored in the Access database, Query 1 and 2 would have to be saved as a dataframes, and the required tables from Access also, then set-up joins to create query three
-        ## could also just read in query 3 (above)
-
         #------------------------------------------------------------------------------------------------
         #1: Step 1 if queries are in Access this step is NOT required, but can be run to VIEW the data; if queries not housed in PD_AoO database this step is neccessary
         qryEUNISFeatAct <- sqlQuery(conn , paste("SELECT tblEUNISFeature.EUNISCode, tblFeatureActivity.FeatSubHabCode, tblFeatureActivity.FARelevant, tblActivityLUT.OperationCode, tblFeatureActivity.ActivityCode, tblActivityLUT.ActivityName 
@@ -94,8 +59,6 @@ read.access.db <- function(db.p = db.path, drv.p = drv.path){
         tblSensitivityLUT <- as.data.frame(sapply(X = tblSensitivityLUT, FUN = as.character), stringsAsFactors=FALSE)
         tblSensitivityLUT$SensPriority <- as.integer(tblSensitivityLUT$SensPriority)
         
-
-        
         #--------------------------
         #TEMPORARY JOIN TABLES/QUERIES - to make above query in case it gets removed from database
         #1 Joins pressure to sensitivity, and (only) allows to filter out by SensPriority
@@ -122,9 +85,41 @@ read.access.db <- function(db.p = db.path, drv.p = drv.path){
         tbl_relevant_activity_pressure_eunis_sens <- left_join(tbl_relevant_activity_pressure_eunis, tmp_jn_tbl_eunis_pressure_sens_lut, by = c("EUNISCode", "PressureCode")) %>%
                 filter(!is.na(ActSensRank)) %>%
                 select(ActivityCode, ActivityName, everything(), -SensPriority)
-
-
-
+        
+        #populate qryEUNIS_ActPressureSens directly from the query in the database, if it fails attempt to reconstruct it from the tables in the database:
+        #qryEUNIS_ActPressSens <- try(sqlQuery(conn, paste("SELECT qryEUNIs_grp_act.ActivityCode, qryEUNIs_grp_act.ActivityName, tblEUNISPressure.PressureCode, tblPressureLUT.PressureName, qryEUNIs_grp_act.EUNISCode, tblEUNISLUT.EUNISName, tblSensitivityLUT.ActSensRank
+        #                                              FROM tblPressureLUT INNER JOIN (tblActivityPressure INNER JOIN ((tblEUNISLUT INNER JOIN qryEUNIs_grp_act ON tblEUNISLUT.EUNISCode = qryEUNIs_grp_act.EUNISCode) INNER JOIN (tblSensitivityLUT INNER JOIN tblEUNISPressure ON tblSensitivityLUT.EPSens = tblEUNISPressure.Sensitivity) ON qryEUNIs_grp_act.EUNISCode = tblEUNISPressure.EUNISCode) ON (tblEUNISPressure.PressureCode = tblActivityPressure.PressureCode) AND (tblActivityPressure.ActivityCode = qryEUNIs_grp_act.ActivityCode)) ON tblPressureLUT.PressureCode = tblActivityPressure.PressureCode
+        #                                                  WHERE (((tblSensitivityLUT.SensPriority)<'8') AND ((tblActivityPressure.APRelevant)='Yes'))
+        #                                                  ORDER BY qryEUNIs_grp_act.ActivityCode, tblEUNISPressure.PressureCode, qryEUNIs_grp_act.EUNISCode;"))) 
+        #if("try-error" %in% class(sqlQuery(conn, paste("SELECT qryEUNIs_grp_act.ActivityCode, qryEUNIs_grp_act.ActivityName, tblEUNISPressure.PressureCode, tblPressureLUT.PressureName, qryEUNIs_grp_act.EUNISCode, tblEUNISLUT.EUNISName, tblSensitivityLUT.ActSensRank
+        #                                              FROM tblPressureLUT INNER JOIN (tblActivityPressure INNER JOIN ((tblEUNISLUT INNER JOIN qryEUNIs_grp_act ON tblEUNISLUT.EUNISCode = qryEUNIs_grp_act.EUNISCode) INNER JOIN (tblSensitivityLUT INNER JOIN tblEUNISPressure ON tblSensitivityLUT.EPSens = tblEUNISPressure.Sensitivity) ON qryEUNIs_grp_act.EUNISCode = tblEUNISPressure.EUNISCode) ON (tblEUNISPressure.PressureCode = tblActivityPressure.PressureCode) AND (tblActivityPressure.ActivityCode = qryEUNIs_grp_act.ActivityCode)) ON tblPressureLUT.PressureCode = tblActivityPressure.PressureCode
+        #                                               WHERE (((tblSensitivityLUT.SensPriority)<'8') AND ((tblActivityPressure.APRelevant)='Yes'))
+        #                                               ORDER BY qryEUNIs_grp_act.ActivityCode, tblEUNISPressure.PressureCode, qryEUNIs_grp_act.EUNISCode;")))) {
+        #        qryEUNIS_ActPressSens <- read.csv("C:/Users/M996613/Phil/PROJECTS/Fishing_effort_displacement/2_subprojects_and_data/3_Other/NE/Habitat_sensitivity/qryhabsens/qryEUNIS_ActPressSens.txt")
+        #}
+        
+        
+        #NB NB NB NB NB NB KEY query!!!!!!!!!!!!!!!!!!!!!!! If IT FAILS DO THE BELOW - nbeeds to be changed to a try statement
+        # 3a if queries are in Access this step is NOT required, but can be run to VIEW the data; else skip to next step
+        #qryEUNIS_ActPressSens <- sqlQuery(conn, paste("SELECT qryEUNIs_grp_act.ActivityCode, qryEUNIs_grp_act.ActivityName, tblEUNISPressure.PressureCode, tblPressureLUT.PressureName, qryEUNIs_grp_act.EUNISCode, tblEUNISLUT.EUNISName, tblSensitivityLUT.ActSensRank
+        #                                              FROM tblPressureLUT INNER JOIN (tblActivityPressure INNER JOIN ((tblEUNISLUT INNER JOIN qryEUNIs_grp_act ON tblEUNISLUT.EUNISCode = qryEUNIs_grp_act.EUNISCode) INNER JOIN (tblSensitivityLUT INNER JOIN tblEUNISPressure ON tblSensitivityLUT.EPSens = tblEUNISPressure.Sensitivity) ON qryEUNIs_grp_act.EUNISCode = tblEUNISPressure.EUNISCode) ON (tblEUNISPressure.PressureCode = tblActivityPressure.PressureCode) AND (tblActivityPressure.ActivityCode = qryEUNIs_grp_act.ActivityCode)) ON tblPressureLUT.PressureCode = tblActivityPressure.PressureCode
+        #                                              WHERE (((tblSensitivityLUT.SensPriority)<'8') AND ((tblActivityPressure.APRelevant)='Yes'))
+        #                                              ORDER BY qryEUNIs_grp_act.ActivityCode, tblEUNISPressure.PressureCode, qryEUNIs_grp_act.EUNISCode;"))
+        #qryEUNIS_ActPressSens <- as.data.frame(sapply(X = qryEUNIS_ActPressSens, FUN = as.character), stringsAsFactors=FALSE)
+        
+        
+        #-------------------
+        ## Inspect data
+        ## List of Tables
+        #subset(sqlTables(conn), TABLE_TYPE == "TABLE") %>%
+        #        arrange(TABLE_NAME)
+        ## List of VIEW queries
+        #subset(sqlTables(conn), TABLE_TYPE == "VIEW")%>%
+        #        arrange(TABLE_NAME)
+        
+        
+        ## All below "building" queries, included, but only query 3 required, as long as the queries are stored in the Access database. IF not stored in the Access database, Query 1 and 2 would have to be saved as a dataframes, and the required tables from Access also, then set-up joins to create query three
+        ## could also just read in query 3 (above)
         
         
 }
