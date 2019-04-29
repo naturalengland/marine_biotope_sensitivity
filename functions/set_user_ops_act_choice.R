d7cb620f8d10feadeec84f3ff55c69d5d33791cb
Philip Haupt <philip.haupt@gmail.com>
2019-04-29 15:14:21 +0100

    further add to message

--- functions/set_user_ops_act_choice.R
+++ functions/set_user_ops_act_choice.R
@@ -2,6 +2,6 @@
 
 choice <- OpsAct %>% filter(OperationCode == ops.number)
 print(paste0("Your choice, Operation Code: ", choice$OperationCode, ", operation name: ", 
-      choice$OperationName, ", Main Activity Code: ", choice$MainActCode))
+      choice$OperationName, ", Main Activity Code: ", choice$MainActCode, ". All activities within ",choice$MainActCode ," will be inlcuded."))
 
 main.act.code <- paste0(choice$MainActCode,".")
