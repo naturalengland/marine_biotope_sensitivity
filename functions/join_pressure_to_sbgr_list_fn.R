fcf07ce9f6311f5fa1c7ece0587481d4c80856f2
Philip Haupt <philip.haupt@gmail.com>
2019-07-05 09:38:17 +0100

    find the position of columns for reordering based on their position relative to "sbgr"

--- functions/join_pressure_to_sbgr_list_fn.R
+++ functions/join_pressure_to_sbgr_list_fn.R
@@ -9,8 +9,15 @@ xap.ls <- act.press.list %>%
                 sbgr.bap <- sbgr.matched.btpt.w.rpl %>% 
                         plyr::llply(function(x) { 
                                 
-                                x.df <- x %>% dplyr::select(eunis.code.gis, sbgr, 1:14, 19:354) %>%
-                                        tidyr::gather("A5.22":"A5.7211",key = "eunis.assessed",value = "eunis.gis") %>%
+                                #to reorder the data, and then compile a dataframe
+                                
+                                # find the position of columns based on their names
+                                sbgr.pos <- grep("sbgr", colnames(x))
+                                total.columns <- ncol(x)
+                                
+                                
+                                x.df <- x %>% dplyr::select(eunis.code.gis, sbgr, 1:eval(sbgr.pos), eval(sbgr.pos):eval(total.columns)) %>%
+                                        tidyr::gather(3:eval(total.columns),key = "eunis.assessed",value = "eunis.gis") %>%
                                         filter(eunis.gis != "<NA>" | eunis.gis != "NA") %>%
                                         select(sbgr, eunis.code.gis, eunis.match.assessed = eunis.assessed) %>%
                                         arrange(sbgr, eunis.code.gis,eunis.match.assessed)
