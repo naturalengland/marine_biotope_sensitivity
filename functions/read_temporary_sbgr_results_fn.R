89aac5fba5de385e2edb32077ac63e5abb4d8192
Philip Haupt <philip.haupt@gmail.com>
2019-07-29 10:29:25 +0100

    minor change: ensure sub-bioregional codes are read in as characters to avoid some being numbers and others being integers.

--- functions/read_temporary_sbgr_results_fn.R
+++ functions/read_temporary_sbgr_results_fn.R
@@ -1,13 +1,14 @@
 #read in all the restuls generated in a single file as lists of dataframes
 results.files <- list.files(folder, full.names = F, recursive=T) %>%
         plyr::ldply(function(x){
-                read.csv(paste0(folder,x), stringsAsFactors=FALSE) %>%
+                csv_file_in <- read.csv(paste0(folder,x), stringsAsFactors=FALSE) #%>%
+                csv_file_in$sbgr <- as.character(csv_file_in$sbgr)
                         #---------
                 #dplyr::mutate(#subBGR = metadata[2],
                 #btp = metadata[7],
                 #eunis.mapped = metadata[10]) %>%
                 #---------
-                dplyr::select(-one_of("X")) # removes the X column
+                csv_file_in %>% dplyr::select(-one_of("X")) # removes the X column
                 
         }) %>%
         plyr::dlply(.(sbgr), identity) # then regroups the data into a list of dataframes according to sub-biogeographic regions; plyr::dlply(.(sbgr,h.lvl), identity)
