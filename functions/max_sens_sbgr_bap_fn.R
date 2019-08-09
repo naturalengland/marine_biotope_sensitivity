23f9c7cf3238ada08498d4a102ac11ae61df8472
Philip Haupt <philip.haupt@gmail.com>
2019-08-09 15:57:59 +0100

    minor . to _

--- functions/max_sens_sbgr_bap_fn.R
+++ functions/max_sens_sbgr_bap_fn.R
@@ -11,16 +11,16 @@ sbgr.BAP.max.sens <- xap.ls %>%
                                 
                                 #filter for maximum sensitivity        
                                 all_sens_per_mapped_eunis <- as.tibble(y) %>% 
-                                        group_by(eunis.code.gis, PressureCode) %>% 
+                                        group_by(eunis_code_gis, PressureCode) %>% 
                                         dplyr::filter(rank.value == min(rank.value)) %>% #the lowest rank number has the highest sensitivity in the Access database - so, this was changed from a max filter to a min filter! (I used supply a custom table, but now it is being read from the Access db to avoid not detecting a change in the system)
                                         ungroup() %>% 
-                                        select(ActivityCode, sbgr, PressureCode, eunis.code.gis, eunis.match.assessed, max.sens = rank.value, biotope_level)%>%#, # maximum sensitivity value, done using mutate to preserve the "eunis.match.assessed" column
-                                        arrange(PressureCode, eunis.code.gis, eunis.match.assessed)
+                                        select(ActivityCode, sbgr, PressureCode, eunis_code_gis, eunis.match.assessed, max.sens = rank.value, biotope_level)%>%#, # maximum sensitivity value, done using mutate to preserve the "eunis.match.assessed" column
+                                        arrange(PressureCode, eunis_code_gis, eunis.match.assessed)
                                 
                                 # filter by assessed biotope of lowest EUNIS level - as this will make fewer assumption when being assigned.
                                 max.sens.tbl <- all_sens_per_mapped_eunis %>% 
-                                        filter(!is.na(eunis.code.gis)) %>% 
-                                        group_by(PressureCode, eunis.code.gis, max.sens) %>% 
+                                        filter(!is.na(eunis_code_gis)) %>% 
+                                        group_by(PressureCode, eunis_code_gis, max.sens) %>% 
                                         dplyr::filter(biotope_level == min(biotope_level)) %>% 
                                         slice(1) %>% 
                                         ungroup() 
@@ -37,8 +37,8 @@ sbgr.BAP.max.sens <- xap.ls %>%
 
 #REMOVE - this produced incorrect result - the slice method to obtain one record per grouping cuased problems - the max_sens was not the corresponding value with the biotope selected
 #max.sens.tbl <- as.tibble(y) %>% 
-#        dplyr::group_by(eunis.code.gis, PressureCode) %>% # want to obtain the minimum and maximum values (comparing betwween eunis.match.assessed) for each for each eunis.code.gis within its sbgr (data is grouped by sbgr, so no need to group_by sbgr here)
+#        dplyr::group_by(eunis_code_gis, PressureCode) %>% # want to obtain the minimum and maximum values (comparing betwween eunis.match.assessed) for each for each eunis_code_gis within its sbgr (data is grouped by sbgr, so no need to group_by sbgr here)
 #        dplyr::mutate(max.sens = max(rank.value))%>%
-#        select(ActivityCode, sbgr, PressureCode, eunis.code.gis, eunis.match.assessed, max.sens)%>%#, # maximum sensitivity value, done using mutate to preserve the "eunis.match.assessed" column
+#        select(ActivityCode, sbgr, PressureCode, eunis_code_gis, eunis.match.assessed, max.sens)%>%#, # maximum sensitivity value, done using mutate to preserve the "eunis.match.assessed" column
 #        slice(1)%>%
 #       ungroup() %>% 
