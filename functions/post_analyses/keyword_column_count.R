31c9587f576044bcdc8091c3dee573aa4efe0193
Philip Haupt <philip.haupt@gmail.com>
2019-11-14 14:38:12 +0000

    function that counts a keyword in a database

--- functions/post_analyses/keyword_column_count.R
+++ functions/post_analyses/keyword_column_count.R
@@ -8,3 +8,7 @@ keyword_column_count <- function(x, keyword = "sens"){
 
 #x expects a data.frame or similar, and keyword is the word entered wihch will be searched for in the columns
 
+#function that counts the number of columns in data frame witha  keywords, like sens: The idea is taht it could count the number of senstivity assessments
+keyword_column_names <- function(x, keyword = "sens"){
+        x %>% st_set_geometry(NULL) %>% select(contains(eval(keyword))) %>% names()
+}
