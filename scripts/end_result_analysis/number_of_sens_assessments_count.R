9d357b9c7028a0c6c1ee5e74afcf7c786da65976
Philip Haupt <philip.haupt@gmail.com>
2019-11-14 14:34:53 +0000

    update aims, libraries

--- scripts/end_result_analysis/number_of_sens_assessments_count.R
+++ scripts/end_result_analysis/number_of_sens_assessments_count.R
@@ -1,24 +1,26 @@
 # count the number of sensitivity assessments, biotopes assessed and confidence assessments 
+# aim: how many data columns
 
-source("./functions/post_analyses/keyword_column_count.R") #this reads in the function that will count the number of columns, based on thedata and keyword
+library(sf)
+library(tidyverse)
+# #this reads in the function that will count the number of columns, based on the data and keyword
+source("./functions/post_analyses/keyword_column_count.R") 
+
+
+#read mapped outputs into R
+filtered_biotope_sens <- read_sf("./outputs/habitat_sensitivity_fishing_multiple_sbgr_mosaic.gpkg")
+unfiltered_biotope_sens_inshore <- read_sf(dsn = "./outputs/habitat_sensitivity_fishing_mosaic_unfiltered.gpkg", layer = "inshore_fishing_ops_inshore_sens_unfiltered")
+unfiltered_biotope_sens_offshore <- read_sf(dsn = "./outputs/habitat_sensitivity_fishing_mosaic_unfiltered.gpkg", layer = "offshore_fishing_ops_inshore_sens_unfiltered")
+
+#user specification: Choose which map to include:
+map_dat <- unfiltered_biotope_sens_offshore # USER INPUT: DECIDE WHICH ONE TO PROCESS
+        #rm(unfiltered_biotopte_sens, filtered_biotopte_sens)
 
 # Results
-## Filtered biotope (inshore)
-# i.e. number of sitivity assessments, biotopes assessed and confidence assessments for all fishing activities
-keyword_column_count(x = filtered_biotope_sens, keyword = "sens") #columns 
-keyword_column_count(x = filtered_biotope_sens, keyword = "assess")
-keyword_column_count(x = filtered_biotope_sens, keyword = "conf")
+## i.e. number of sitivity assessments, biotopes assessed and confidence assessments for all fishing activities
+keyword_column_count(x = map_dat, keyword = "sens") # number of columns containing senstivity assessments
+keyword_column_count(x = map_dat, keyword = "assess") # number of columns containing assessed biotopte info
+keyword_column_count(x = map_dat, keyword = "conf") # number of columns containingconfidence assessments
 
-# Results
-## Unfiltered biotope - INSHORE
-# i.e. number of sitivity assessments, biotopes assessed and confidence assessments for all fishing activities
-keyword_column_count(x = unfiltered_biotope_sens_inshore, keyword = "sens") #columns 
-keyword_column_count(x = unfiltered_biotope_sens_inshore, keyword = "assess")
-keyword_column_count(x = unfiltered_biotope_sens_inshore, keyword = "conf")
-
-# Results
-## Unfiltered biotope - OFFSHORE
-# i.e. number of sitivity assessments, biotopes assessed and confidence assessments for all fishing activities
-keyword_column_count(x = unfiltered_biotope_sens_offshore, keyword = "sens") #columns 
-keyword_column_count(x = unfiltered_biotope_sens_offshore, keyword = "assess")
-keyword_column_count(x = unfiltered_biotope_sens_offshore, keyword = "conf")
+# to inspect - call the second function in the keyword search function:
+keyword_column_names(x = map_dat, keyword = "sens")
