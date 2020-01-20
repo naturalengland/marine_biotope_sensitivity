35fb69c69e2a5adf02c666bc30eed3947d5b11f6
Philip Haupt <philip.haupt@gmail.com>
2020-01-20 10:10:37 +0000

    modify list to be more comprehensive and tabled

--- scripts/list_of_functions/my_libraries_used.R
+++ scripts/list_of_functions/my_libraries_used.R
@@ -2,18 +2,43 @@
 library(NCmisc)
 library(tidyverse)
 
-#list of libraries used in main script
-list.functions.in.file(filename = "./scripts/main_script.R", alphabetic = TRUE)
+#list of libraries used in "main script"
+main_script_packages <-
+        list.functions.in.file(filename = "./scripts/main_script.R", alphabetic = TRUE) %>%
+        names() %>%
+        unique() %>%
+        as_tibble() %>%
+        dplyr::rename(packages = value) %>%
+        dplyr::filter(str_detect(string = .$packages, pattern = "^package:")) %>%
+        unlist() %>%
+        stringr::str_replace(pattern = "package:", replacement = "")
 
 # list of functions in functions folder
 my_functions <- list.files("./functions/", pattern = "\\.R$")
 
-# Check libraries used in my functions
-
+# Check packages used in my helper functions
 my_full_fns_ls <- list(list())
 for (i in seq_along(my_functions)) {
-        my_ith_fns_ls <- list.functions.in.file(filename = paste0("./functions/",my_functions[i]), alphabetic = TRUE)
-        packages_tmp <- unlist(my_ith_fns_ls) %>% names()
+        my_ith_fns_ls <-
+                list.functions.in.file(
+                        filename = paste0("./functions/", my_functions[i]),
+                        alphabetic = TRUE
+                )
+        packages_tmp <- names(my_ith_fns_ls) %>% unique() %>%
+                stringr::str_replace("package:", "")
         
-        my_full_fns_ls[i] <- my_ith_fns_ls
-        }
+        my_full_fns_ls[[i]] <-
+                packages_tmp %>% as_tibble() %>%
+                dplyr::rename(packages = value) %>%
+                filter(!str_detect(string = .$packages, pattern = "^c"))
+        
+} 
+#combine main script and list of packages from helper functions
+all_packages_used <- my_full_fns_ls %>% unlist() %>% 
+        as.character()  %>% 
+        c(main_script_packages) %>% 
+        unique() %>% 
+        as_tibble() %>% 
+        dplyr::rename(packages = value) %>% 
+        dplyr::filter(!str_detect(string = .$packages,pattern = ".Global"))
+write_rds(all_packages_used, "./data/all_packages_used.R")
